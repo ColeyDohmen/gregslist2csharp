2bab37dbb01c4cedfc5a070fd23fd4628c7d2a48
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-07 13:43:41 -0600

    controller set up

--- Services/CarService.cs
+++ Services/CarService.cs
@@ -1,7 +1,33 @@
+using System;
+using gregslist2.Models.cs;
+
 namespace gregslist2.Services
 {
     public class CarService
     {
-        
+        internal object Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Create(Car newCar)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object EditCar(Car editCar)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
